e6b02f5862048829fbfb81f20c02257802d7d4f8
praisejahuzoma <praisejah.c.uzoma@gmail.com>
2021-08-05 21:42:43 +0100

    finish skill page

--- js/script.js
+++ js/script.js
@@ -24,3 +24,23 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// skills
+
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHearder = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHearder.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
