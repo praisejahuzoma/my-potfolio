63a6bfe12ff4194392bb707445fc1f6ea11ae885
praisejahuzoma <praisejah.c.uzoma@gmail.com>
2021-08-13 14:13:57 +0100

    qualification,services,skills

--- js/script.js
+++ js/script.js
@@ -44,3 +44,61 @@ function toggleSkills(){
 skillsHearder.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
+
+// qualification
+const tabs = document.querySelectorAll('[data-target]'),
+      tabsContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabsContents.forEach(tabsContents =>{
+            tabsContents.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
+
+// services
+const modalViews = document.querySelectorAll('.services_modal'),
+      modalBtns = document.querySelectorAll('.services_button'),
+      modalCloses = document.querySelectorAll('.services_modal-close')
+   
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}      
+
+modalBtns.forEach((modalBtns, i) => {
+    modalBtns.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+// portoflio swiper
+let swiper = new Swiper('.portfolio_container', {
+    cssMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
